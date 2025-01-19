0acc73abc5b40e098fa8ea2aafd0e73d17b57d30
Ubuntu <ubuntu@ip-172-31-10-111.ap-south-1.compute.internal>
2025-01-19 13:25:08 +0000

    I have added arithmatic file

---
+++ Day-2/aithmatic1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#!/bin/bash
+
+var1=100
+var2=50
+var3=25
+
+v1=$((var1 + var2))  # Correct syntax for addition
+v2=$((var2 - var3))  # Correct syntax for subtraction
+v3=$((var1 * var3))  # Correct syntax for multiplication
+
+echo "The addition is $v1"
+echo "The subtraction is $v2"
+echo "The multiplication is $v3"
+
